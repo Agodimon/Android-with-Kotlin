bf3e7d16b3974f594393a0111b8b34b7ba0aacf7
dima <agodimon@mail.ru>
2021-08-28 12:37:39 +0500

    делаю дз№7
    1. Используйте Retrofit для получения данных с сервера.
    
    подключил библиотеку Retrofit.

--- app/src/main/java/com/bignerdranch/android/androidwithkotlin/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/bignerdranch/android/androidwithkotlin/model/repository/RepositoryImpl.kt
@@ -3,10 +3,11 @@ package com.bignerdranch.android.androidwithkotlin.model.repository
 import com.bignerdranch.android.androidwithkotlin.model.WeatherLoader
 import com.bignerdranch.android.androidwithkotlin.model.entities.City
 import com.bignerdranch.android.androidwithkotlin.model.entities.Weather
+import com.bignerdranch.android.androidwithkotlin.model.rest.WeatherRepo
 
 class RepositoryImpl : Repository {
        override fun getWeatherFromServer(lat: Double, lng: Double): Weather {
-        val dto = WeatherLoader.loadWeather(lat, lng)
+        val dto = WeatherRepo.api.getWeather(lat, lng).execute().body()
         return Weather(
             temperature = dto?.fact?.temp ?: 0,
             feelsLike = dto?.fact?.feelsLike ?: 0,
