e776798ee18e1271f4878208070aff392417a05c
dima <agodimon@mail.ru>
2021-08-29 21:19:55 +0500

    приступаю к дз №8 продолжаем писать приложение "погода"

--- app/src/main/java/com/bignerdranch/android/androidwithkotlin/framework/ui/main/MainViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/androidwithkotlin/framework/ui/main/MainViewModel.kt
@@ -15,19 +15,20 @@ class MainViewModel(private val repository: Repository) : ViewModel(), Lifecycle
 
     fun getWeatherFromLocalSourceRus() = getDataFromLocalSource(isRussian = true)
     fun getWeatherFromLocalSourceWorld() = getDataFromLocalSource(isRussian = false)
-   // fun getWeatherFromRemoteSource() = getDataFromLocalSource(isRussian = true)
+    // fun getWeatherFromRemoteSource() = getDataFromLocalSource(isRussian = true)
 
-    private fun getDataFromLocalSource(isRussian : Boolean) {
+    private fun getDataFromLocalSource(isRussian: Boolean) {
         liveDataToObserve.value = AppState.Loading
         Thread {
             sleep(1000)
 
             liveDataToObserve.postValue(
-                if(isRussian){
+                if (isRussian) {
                     AppState.Success(repository.getWeatherFromLocalStorageRus())
-                } else{
+                } else {
                     AppState.Success(repository.getWeatherFromLocalStorageWorld())
-                })
+                }
+            )
         }.start()
     }
 
