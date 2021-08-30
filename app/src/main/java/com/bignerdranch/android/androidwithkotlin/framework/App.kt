37b7b3d2b5f16683942039ffbaab4a63d7af0b55
dima <agodimon@mail.ru>
2021-08-31 01:53:06 +0500

    Создаю базу данных

--- app/src/main/java/com/bignerdranch/android/androidwithkotlin/framework/App.kt
+++ app/src/main/java/com/bignerdranch/android/androidwithkotlin/framework/App.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.androidwithkotlin.framework
 
 import android.app.Application
+import android.content.Context
 import com.bignerdranch.android.androidwithkotlin.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -13,4 +14,10 @@ class App : Application() {
             modules(appModule)
         }
     }
+
+    companion object {
+        lateinit var appContext: Context
+    }
+
 }
+
