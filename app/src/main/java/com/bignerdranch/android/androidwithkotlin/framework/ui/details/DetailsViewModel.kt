b9d0a4788317d12a94bd3c63b636cba4cca2c071
dima <agodimon@mail.ru>
2021-09-01 19:02:33 +0500

    Практику по ДЗ №8 выполнил. Всё работает отлично. Название городов в истории запросов сохраняются верно.

--- app/src/main/java/com/bignerdranch/android/androidwithkotlin/framework/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/androidwithkotlin/framework/ui/details/DetailsViewModel.kt
@@ -10,10 +10,13 @@ import com.bignerdranch.android.androidwithkotlin.model.repository.Repository
 class DetailsViewModel(private val repository: Repository) : ViewModel(), LifecycleObserver {
     val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData()
 
-    fun loadData(lat: Double, lng: Double) {
+    fun loadData(cityName:String, lat: Double, lng: Double) {
         liveDataToObserve.value = AppState.Loading
         Thread {
-            val data = repository.getWeatherFromServer(lat, lng)
+            val data = repository.getWeatherFromServer(lat, lng).apply {
+                city.city = cityName
+            }
+
             repository.saveEntity(data)
             liveDataToObserve.postValue(AppState.Success(listOf(data)))
         }.start()
